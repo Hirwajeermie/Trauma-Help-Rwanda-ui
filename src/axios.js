22420927f9aefd004105ba4d2f861e4733d7c9f9
hirwa <hirwajeermie@gmail.com>
2024-09-23 19:38:04 +0200

    modifaid mistake

--- src/axios.js
+++ src/axios.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 import { store } from './store';
 
 const api = axios.create({
-  baseURL: 'https://trauma-help-rwanda-api.onrender.com/api/v1',
+  baseURL: 'http://localhost:7000/api/v1',
 });
 
 api.interceptors.request.use((config) => {
