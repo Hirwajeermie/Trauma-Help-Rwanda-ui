91b68775848ad573f18d40d402f53d1ac2123b01
Jeermie <148848399+Hirwajeermie@users.noreply.github.com>
2025-07-18 15:53:48 -0700

    lastpush

--- src/Components/Introd/Introd.jsx
+++ src/Components/Introd/Introd.jsx
@@ -10,9 +10,10 @@ const Introd = () => {
     <div className='Introd'>
         <div className='Introd-text'>
             <h1>TRAUMA HELP RWANDA </h1>
-            <p>Trauma Help Rwanda (THR) is a NATIONAL NON-GOVERNMENTAL ORGANISATION 
-                recognized by Rwanda Government Board (RGB), registered by on No 
-                  85/RGB/ NGO/2017.</p>
+            <p>Trauma Help Rwanda (THR) is a legally registered non-governmental organization recognized by the Rwanda 
+              Governance Board (RGB) under Law No 85/RGB/NGO/2017. THR is committed to addressing the silent of 
+              psychological trauma across communities affected by the tragic history of the 1994 
+              Genocide against the Tutsi, intergenerational trauma as well as post-conflict experiences..</p>
                   <button onClick={() => navigate('./ExploreMore')} className='btn'>Explore More</button>
         </div>
       
--- src/Components/Whatwedo/Whatwedo.jsx
+++ src/Components/Whatwedo/Whatwedo.jsx
@@ -13,35 +13,62 @@ const Whatwedo = () => {
     <div id='Whatwedo'>
        <div className='Title'>
       <h3>What We Do</h3>
-        <h1>Our VISION And MISSION</h1>
-        <p>Rwandan society is aware of mental health issues, especially psychological 
-          Trauma problems, <br /> takes care of them and receives high quality professional services. <br />
+        <h1>Our MISSION</h1>
+        <p>To provide professional counseling services to Rwandan society, which has experienced trauma related
+           to the<br /> 1994 Genocide against the Tutsi and its consequences. The focus is on preventing new causes of<br />
+            psychological trauma, offering treatment for existing trauma, stress management and <br /> burnout,
+            and other trauma-related disorders such as depression,<br /> grief, drug abuse etc.   <br />
 
           <br />
-
-          Doing counseling services for Rwandese people presenting symptoms of psychological
-           trauma in which <br /> the most time are caused by genocide against Tutsis of 1994 as well 
-           as its consequences and preventing the <br /> new causes of Psychological Trauma in Rwanda community.
+       </p>
+       <h1>Our VISION </h1>
+       <p>
+        A Rwandan society that is aware of mental health issues especially psychological trauma 
+        actively<br /> addresses them and has access to high-quality professional services
        </p>
     </div>
 
    <div className='What-we-do'>
       <div className='What-wedo'>
         <img src={picture1} alt="" />
-        <p>Conversations</p>
+        <p></p>Our efforts focus on raising awareness about mental health issues particularly 
+        psychological trauma, providing basic psychosocial counseling services, and encouraging 
+        individuals to access available mental health services, especially 
+        those offered at health centers.
       </div>
       <div className='What-wedo'>
         <img src={picture2} alt="" />
-        <p>Conversations</p>
+        <p>At the community level, we work closely with community actors from the seven administrative 
+          sectors of Nyanza District, including:<br />
+          Representatives of community health workers (Abajyanama bubuzima), Religious Leaders (Abahagarariye amadini n’amatorero), 
+          </p>
       </div>
       <div className='What-wedo'>
         <img src={picture3} alt="" />
-        <p>Conversations</p>
+        <p>
+        Trauma Help Rwanda (THR) enhances the effectiveness of psychological trauma counseling by 
+        increasing skills of mental health providers in trauma treatment. It also offers InterVision 
+        sessions and clinical supervision to mental health professionals in 10 health centers across 
+        Nyanza District.
+        </p>
       </div>
       <div className='What-wedo'>
         <img src={picture4} alt="" />
-        <p>Conversations</p>
+        <p>THR supports mental health in schools through the following initiatives:<br />
+           Training teachers and student leaders on the symptoms of trauma and the basics of trauma counseling including active listening.
+        </p>
       </div>
+    </div>
+    <div className='Title'>
+      <h2> Objectives </h2>
+      <ul>
+        <li>•	Deliver psychosocial counseling, stress management, and burnout prevention services.</li>
+        <li>•	Combat psychological trauma and raise awareness across Rwanda.</li>
+        <li>• Build local capacity through training and supervision on trauma-informed care</li>
+        <li>•	Promote Alternative Dispute Resolution (ADR) in conflict management.</li>
+        <li>•	Foster community unity and reconciliation.</li>
+        <li>•	Prevent the transmission of trauma across generations.</li>
+      </ul>
     </div>
     <div>
     <button onClick={() => navigate('./Report')} className='btn black-btn'>Report</button>
